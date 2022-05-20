fb8e168da94e09ecd04fe13b567daefc04c43d05
ElenaStepanchuk <podolyak365@gmail.com>
2022-05-20 22:51:39 +0300

    PersistGate

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,33 @@
 import { configureStore } from '@reduxjs/toolkit';
 import contactsReducer from './contacts/contactsReducer';
 import { filterReducer } from '../redux/contacts/contactsReducer';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+const persistConfig = {
+  key: 'contacts',
+  storage,
+  whitelist: ['contacts'],
+};
+const persistedContactsReducer = persistReducer(persistConfig, contactsReducer);
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistedContactsReducer,
     filter: filterReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+export const persistor = persistStore(store);
