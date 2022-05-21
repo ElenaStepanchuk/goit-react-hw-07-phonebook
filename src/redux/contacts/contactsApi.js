a7d78e8d09fe3a20f21fd2382a1e438ee302a13f
ElenaStepanchuk <podolyak365@gmail.com>
2022-05-21 20:14:52 +0300

    axios

--- src/redux/contacts/contactsApi.js
+++ src/redux/contacts/contactsApi.js
@@ -1,14 +1,17 @@
 import axios from 'axios';
+axios.defaults.baseURL = `https://627e6e64271f386ceff80667.mockapi.io`;
+
 export const fetchContacts = async () => {
-  const { data } = await axios.get(
-    'https://627e6e64271f386ceff80667.mockapi.io/api/v1/contacts'
-  );
+  const { data } = await axios.get('/api/v1/contacts');
   return data;
 };
 
-export const fetchAddContacts = async ({ contact }) => {
-  const { data } = await axios.get(
-    `https://627e6e64271f386ceff80667.mockapi.io/api/v1/contacts?filter=${contact}`
-  );
+export const fetchAddContacts = async ({ name, number, id }) => {
+  const { data } = await axios.post(`/api/v1/contacts`, {
+    name: `${name}`,
+    number: `${number}`,
+    id: `${id}`,
+  });
+  console.log(data);
   return data;
 };
