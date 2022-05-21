88ec6ba9a02df28bab45e62e638e94796d3ca7dc
ElenaStepanchuk <podolyak365@gmail.com>
2022-05-21 23:08:55 +0300

    correct axios fetch

--- src/redux/contacts/contactsApi.js
+++ src/redux/contacts/contactsApi.js
@@ -1,17 +1,31 @@
-import axios from 'axios';
-axios.defaults.baseURL = `https://627e6e64271f386ceff80667.mockapi.io`;
+// import axios from 'axios';
+// axios.defaults.baseURL = `https://627e6e64271f386ceff80667.mockapi.io`;
 
-export const fetchContacts = async () => {
-  const { data } = await axios.get('/api/v1/contacts');
-  return data;
-};
+// export const fetchContacts = async () => {
+//   const { data } = await axios.get('/api/v1/contacts?sortby=name');
+//   return data;
+// };
 
-export const fetchAddContacts = async ({ name, number, id }) => {
-  const { data } = await axios.post(`/api/v1/contacts`, {
-    name: `${name}`,
-    number: `${number}`,
-    id: `${id}`,
-  });
-  console.log(data);
-  return data;
-};
+// export const fetchAddContacts = async ({ name, number, id }) => {
+//   const { data } = await axios.post(
+//     `/api/v1/contacts?sortby=name&search=${name}`,
+//     {
+//       name: `${name}`,
+//       number: `${number}`,
+//       id: `${id}`,
+//     }
+//   );
+//   console.log(data);
+//   return data;
+// };
+
+// export const fetchFindContacts = async ({ name }) => {
+//   const { data } = await axios.post(
+//     `/api/v1/contacts?sortby=name&search=${name}`,
+//     {
+//       name: `${name}`,
+//     }
+//   );
+//   console.log(data);
+//   return data;
+// };
--- src/redux/contacts/contactsOperations.js
+++ src/redux/contacts/contactsOperations.js
@@ -1,11 +1,18 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import * as contactsAPI from './contactsApi';
+// import * as contactsAPI from './contactsApi';
+import axios from 'axios';
+axios.defaults.baseURL = `https://627e6e64271f386ceff80667.mockapi.io`;
+
+// export const allContacts = async () => {
+//   const { data } = await axios.get('/api/v1/contacts?sortby=name');
+//   return data;
+// };
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchContacts',
   async (_, { rejectWithValue }) => {
     try {
-      const contacts = await contactsAPI.fetchContacts();
-      return contacts;
+      const { data } = await axios.get('/api/v1/contacts?sortby=name');
+      return data;
     } catch (error) {
       return rejectWithValue(error);
     }
